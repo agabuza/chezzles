f4c63a86c38bc511fbe8728a42842a5c3468d136
agrocks <gabuzaav@gmail.com>
2015-11-09 02:47:23 +0100

    Cocossharp update to 1.7.0.0-pre (supporting Xamarin.Forms);

--- chezzles.cocossharp/chezzles.cocossharp/chezzles.cocossharp/GameLayer.cs
+++ chezzles.cocossharp/chezzles.cocossharp/chezzles.cocossharp/GameLayer.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CocosSharp;
 using chezzles.cocossharp.Pieces;
-using chezzles.engine.Pieces;
-using chezzles.engine.Core;
 using chezzles.engine.Data;
 using System.Linq;
 using chezzles.cocossharp.Extensions;
-using chezzles.cocossharp.Menu;
-using System.Diagnostics;
 using chezzles.engine.Core.Game;
 
 namespace chezzles.cocossharp
@@ -20,56 +15,49 @@ namespace chezzles.cocossharp
         private float scaleFactor = 4;
 
         // change hardcoded values to get correct board place
-        public static CCPoint Origin = new CCPoint(0, 400);
-        private CCButton pauseButton;
-        private CCButton nextButton;
-        private CCLabel score;
+        public static CCPoint Origin = new CCPoint(0, 0);
+        //private CCButton pauseButton;
+        //private CCButton nextButton;
         private GamesStorage storage;
         private int index;
 
         public GameLayer(CCSize size)
             : base(size)
         {
-            var touchListener = new CCEventListenerTouchAllAtOnce();
-            touchListener.OnTouchesEnded = OnTouchesEnded;
-
-            AddEventListener(touchListener, this);
             this.pieceBuilder = new CocoPieceBuilder();
-            Color = CCColor3B.Green;
-
             this.storage = new GamesStorage();
         }
 
         private void InitializeMenu()
         {
-            var cache = CCSpriteFrameCache.SharedSpriteFrameCache;
-            cache.AddSpriteFrames("hd/buttons.plist");
+            //var cache = CCSpriteFrameCache.SharedSpriteFrameCache;
+            //cache.AddSpriteFrames("hd/buttons.plist");
+            
+            //this.pauseButton = new CCButton(cache["btn_pause.png"])
+            //           .ScaleToSize(330, 150)
+            //           .PlaceAt(.2f, .9f, 101, this);
 
-            this.pauseButton = new CCButton(cache["btn_pause.png"])
-                       .ScaleToSize(330, 150)
-                       .PlaceAt(.2f, .9f, 101, this);
+            //this.pauseButton.Click += (s) =>
+            //{
+            //};
 
-            this.pauseButton.Click += (s) =>
-            {
-            };
+            //this.nextButton = new CCButton(cache["btn_next.png"])
+            //    .ScaleToSize(330, 150)
+            //    .PlaceAt(.8f, .9f, 101, this);
 
-            this.nextButton = new CCButton(cache["btn_next.png"])
-                .ScaleToSize(330, 150)
-                .PlaceAt(.8f, .9f, 101, this);
+            //this.nextButton.Click += (s) =>
+            //{
+            //    var game = this.storage.Get(++this.index);
+            //    if (game != null)
+            //    {
+            //        ClearBoard();
+            //        DrawBoard(this, game);
+            //    }
+            //};
 
-            this.nextButton.Click += (s) =>
-            {
-                var game = this.storage.Get(++this.index);
-                if (game != null)
-                {
-                    ClearBoard();
-                    DrawBoard(this, game);
-                }
-            };
-
-            this.score = new CCLabel("Score 100% out of 12 puzzles", "arial", 36f)
-                .PlaceAt(.2f, .1f, 101, this)
-                .WithTextCentered();
+            //this.score = new CCLabel("Score 100% out of 12 puzzles", "arial", 36f)
+            //    .PlaceAt(.2f, .1f, 101, this)
+            //    .WithTextCentered();
 
         }
 
@@ -85,7 +73,7 @@ namespace chezzles.cocossharp
         private void AddBoard()
         {
             this.tileMap = new CCTileMap("tilemaps/board.tmx");
-            this.scaleFactor = Layer.VisibleBoundsWorldspace.Size.Width / tileMap.TileLayersContainer.ContentSize.Width;
+            this.scaleFactor = this.ContentSize.Width / tileMap.TileLayersContainer.ContentSize.Width;
             this.tileMap.Scale = scaleFactor;
 
             tileMap.PositionX = Origin.X;
@@ -94,11 +82,14 @@ namespace chezzles.cocossharp
 
             tileMap.Antialiased = false;
             this.AddChild(tileMap, -1);
+
+            var game = this.storage.Get(++this.index);
+            DrawBoard(this, game);
         }
 
         private void DrawBoard(CCNode gameLayer, Game game)
         {
-            game.Board.SetSize(Window.WindowSizeInPixels.Width);
+            game.Board.SetSize(this.tileMap.ContentSize.Width);
             foreach (var piece in game.Board.Pieces)
             {
                 var cocoPiece = this.pieceBuilder.Build(piece);
@@ -110,13 +101,9 @@ namespace chezzles.cocossharp
         protected override void AddedToScene()
         {
             base.AddedToScene();
-            Scene.SceneResolutionPolicy = CCSceneResolutionPolicy.ShowAll;
+            //Scene.SceneResolutionPolicy = CCSceneResolutionPolicy.ShowAll;
             AddBoard();
             this.InitializeMenu();
-        }
-
-        private void OnTouchesEnded(List<CCTouch> arg1, CCEvent arg2)
-        {
         }
     }
 }
---
+++ chezzles.cocossharp/chezzles.cocossharp/chezzles.cocossharp/Views/GamePage.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using CocosSharp;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace chezzles.cocossharp.Views
+{
+    public class GamePage : ContentPage
+    {
+        CocosSharpView gameView;
+        
+        public GamePage()
+        {
+            this.gameView = new CocosSharpView()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                DesignResolution = new Size(100, 100),
+                ViewCreated = LoadGame
+            };
+           
+            Content = gameView;
+        }
+
+        private void LoadGame(object sender, EventArgs e)
+        {
+            var gameView = sender as CCGameView;
+            if (gameView != null)
+            {
+                // Set world dimensions
+                gameView.DesignResolution = new CCSizeI((int)this.Width, (int)this.Height);
+                gameView.ContentManager.SearchPaths =
+                    new List<string>() { "", "hd", "animations", "fonts", "sounds", "images" };
+
+                CCScene gameScene = new CCScene(gameView);
+                gameScene.AddLayer(new GameLayer(new CCSize((float)this.Width, (float)this.Height)));
+                gameView.RunWithScene(gameScene);
+            }
+        }
+    }
+}
