9ffa46477a5af51af13a6148bbf55a72eb73b7be
agrocks <gabuzaav@gmail.com>
2016-02-23 03:47:20 +0100

    Repo added;

--- chezzles.api/chezzles.data/EF/Repository.cs
+++ chezzles.api/chezzles.data/EF/Repository.cs
@@ -1,55 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace chezzles.data.EF
 {
-    public class Repository<T> : IRepository<T> where T : IClientEntity
+    public class Repository<T> : IRepository<T> where T : class, IClientEntity
     {
-        public Repository(IDbDataProvider context)
-        {
+        private DbSet<T> context;
+        private IUnitOfWork unitOfWork;
+        private IDbDataProvider dataProvider;
 
-        }
-        public void Delete(T entity)
+        public Repository(IDbDataProvider dataProvider)
         {
-        }
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> Find(Func<T, bool> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T FirstOrDefault(Func<T, bool> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T GetById(long id)
-        {
-            throw new NotImplementedException();
+            this.context = dataProvider.GetDbSet<T>();
+            this.dataProvider = dataProvider;
+            unitOfWork = dataProvider as IUnitOfWork;
         }
 
         public void Insert(T entity)
         {
+            context.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return context.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return context;
+        }
+
+        public T GetById(long id)
+        {
+            return context.FirstOrDefault(x => x.Id == id);
+        }
+
+        public T FirstOrDefault(Func<T, bool> predicate)
+        {
+            return context.FirstOrDefault(predicate);
+        }
+
+        public void Attach(T entity)
+        {
+            var entry = this.dataProvider.ContextEntry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                var currentEntry = this.context.Find(entity.Id);
+                if (currentEntry != null)
+                {
+                    var attachedEntry = this.dataProvider.ContextEntry(currentEntry);
+                    attachedEntry.CurrentValues.SetValues(entity);
+                }
+                else
+                {
+                    this.context.Attach(entity);
+                    entry.State = EntityState.Modified;
+                }
+            }
+        }
+
+        private object GetPrimaryKey(DbEntityEntry<T> entry)
+        {
             throw new NotImplementedException();
+        }
+
+        #region IUnitOfWork Members
+
+        public void Save()
+        {
+            if (unitOfWork != null)
+                unitOfWork.Save();
+        }
+
+        public Task SaveAsync()
+        {
+            if (unitOfWork != null)
+                 return unitOfWork.SaveAsync();
+
+            return Task.Delay(0);
         }
 
         public void Update(T entity)
         {
             throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
---
+++ chezzles.cocossharp/ViewModels/PuzzleViewModel.cs
@@ -0,0 +1,45 @@
+﻿using chezzles.engine.Core.Game.Messages;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chezzles.cocossharp.ViewModels
+{
+    public class PuzzleViewModel : ViewModelBase
+    {
+        public int failedCount = 0;
+        public int solvedCount = 0;
+
+        public PuzzleViewModel(IMessenger messenger)
+        {
+            this.MessengerInstance = messenger;
+            RegisterForMessages(messenger);
+        }
+
+        public string Status
+        {
+            get
+            {
+                return $"Solved: {this.solvedCount} Failed: {this.failedCount}";
+            }
+        }
+
+
+        private void RegisterForMessages(IMessenger messenger)
+        {
+            MessengerInstance.Register<PuzzleCompletedMessage>(this, (msg) =>
+            {
+                if (msg.IsSolved)
+                    this.solvedCount++;
+                else
+                    this.failedCount++;
+
+                RaisePropertyChanged("Status");
+            });
+        }
+    }
+}
